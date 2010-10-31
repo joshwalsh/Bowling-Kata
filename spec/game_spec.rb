c63200c66add7a6ba90a46de6deb8b69a96fe9d6
Josh Walsh <jwalsh@designinginteractive.com>
2010-10-30 21:41:30 -0400

    Roll many test helper

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,13 +3,17 @@ require 'game'
 describe Game do
   subject { Game.new }
 
+  def roll_many(n, pins)
+    n.times { subject.roll pins }
+  end
+
   it "scores 0 for gutter game" do
-    20.times { subject.roll 0 }
+    roll_many 20, 0
     subject.score.should == 0
   end
   
   it "scores 20 for all 1's" do
-    20.times { subject.roll 1 }
+    roll_many 20, 1
     subject.score.should == 20
   end
 end
