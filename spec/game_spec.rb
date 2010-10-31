3dc200c63ed4314a3d214fa3822fafb686e04609
Josh Walsh <jwalsh@designinginteractive.com>
2010-10-30 21:39:22 -0400

    Roll all 1's

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,4 +7,9 @@ describe Game do
     20.times { subject.roll 0 }
     subject.score.should == 0
   end
+  
+  it "scores 20 for all 1's" do
+    20.times { subject.roll 1 }
+    subject.score.should == 20
+  end
 end
